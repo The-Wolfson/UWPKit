80d8f2ba43e185ad8bd4c3d425cde7627f939ac1
The-Wolfson <The-Wolfson@users.noreply.github.com>
2026-04-17 22:44:29 +0000

    Commit from GitHub Actions (Generate Swift Bindings)

--- Package.swift
+++ Package.swift
@@ -7,17 +7,17 @@ let package = Package(
     products: [
         .library(name: "WindowsFoundation", type: .dynamic, targets: ["WindowsFoundation"]),
 
-        .library(name: "WindowsStorage", type: .dynamic, targets: ["WindowsStorage"]),
+                .library(name: "WindowsStorage", type: .dynamic, targets: ["WindowsStorage"]),
 
         /// <ADD PACKAGES>
     ],
     targets: [
         .target(name: "WindowsFoundation", dependencies: [
-            .product(name: "CWinRT"),
+            "CWinRT",
         ]),
         .target(name: "CWinRT"),
 
-        .target(name: "WindowsStorage", dependencies: [.product(name: "CWinRT"), .product(name: "WindowsFoundation")]),
+                .target(name: "WindowsStorage", dependencies: ["CWinRT", "WindowsFoundation"]),
 
         /// <ADD PACKAGES>
     ]
